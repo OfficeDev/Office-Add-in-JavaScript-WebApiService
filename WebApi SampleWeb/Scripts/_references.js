2a0bd841f617db3d2be478c17d97aa4a24eb43e8
DianeD <ddiaz99@live.com>
2015-08-13 11:52:20 -0700

    update manifest to 1.1
    updated manifest to 1.1/hosts; removed local jquery reference in
    references.js

--- WebApi SampleWeb/Scripts/_references.js
+++ WebApi SampleWeb/Scripts/_references.js
@@ -1,5 +1,4 @@
-﻿/// <reference path="jquery-1.9.1.js" />
-/* Required to correctly initalize Office.js for intellisense */
+﻿/* Required to correctly initalize Office.js for intellisense */
 /// <reference path="_officeintellisense.js" />
 /* Use offline copy of Office.js for intellisense */
 // /// <reference path="office/1.1/outlook-win32.debug.js" />
